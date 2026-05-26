6bec1a05031b7039f60986d5218678cb896cc13a
Dmitry Reztsov <rez_dima@icloud.com>
2026-05-26 21:25:19 +0300

    Update zailimits to v1.1

--- Casks/zailimits.rb
+++ Casks/zailimits.rb
@@ -1,6 +1,6 @@
 cask "zailimits" do
-  version "1.0"
-  sha256 "PLACEHOLDER"
+  version "1.1"
+  sha256 "000a6771afaa4d88471f25d81105d8b7bcaf6ad82bdc4d0a4fb204d546f67fbc"
 
   url "https://github.com/DmitryReztsov/z-ai-limits/releases/download/v#{version}/ZAILimitsMonitor.zip"
   name "Z.ai Limits"
